9b9a8ca01fbc190780d657a5c5bd785b5a1df560
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-09-10 13:14:20 +0200

    move components inside src

--- packages/react-ui/index.js
+++ packages/react-ui/index.js
@@ -6,9 +6,9 @@ export const Form = create('form', 'Form')
 export const Heading = create('h1', 'Heading')
 export const Paragraph = create('p', 'Paragraph')
 
-export * from './components/button'
-export * from './components/button-group'
-export * from './components/input'
-export * from './components/input-group'
+export * from './src/components/button'
+export * from './src/components/button-group'
+export * from './src/components/input'
+export * from './src/components/input-group'
 
 export { ThemeProvider }
