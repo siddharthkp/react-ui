0acb272113ab57f164cecf774fb01962f9352810
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-12-03 18:03:36 +0100

    omg it worked

--- packages/react-ui/index.js
+++ packages/react-ui/index.js
@@ -1,8 +1,3 @@
-// import { create } from './helpers'
-//
-// export const Heading = create('h1', 'Heading')
-// export const Paragraph = create('p', 'Paragraph')
-
 export * from './src/components/button'
 export * from './src/components/button-group'
 export * from './src/components/input'
---
+++ packages/react-ui/rollup.config.js
@@ -0,0 +1,24 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import external from 'rollup-plugin-peer-deps-external'
+
+import pkg from './package.json'
+
+export default [
+  {
+    input: pkg.source,
+
+    plugins: [
+      external({
+        includeDependencies: true
+      }),
+      resolve(),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      commonjs()
+    ],
+    output: [{ file: pkg.main, format: 'cjs' }]
+  }
+]
