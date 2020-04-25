6b1a3c9169ccb05037decda7634dbe24d788cdf8
Ankit Kumar <ankit.kr.balyan@gmail.com>
2020-04-26 04:37:52 +0530

    Added fullWidth prop to multiple components

--- packages/react-ui/src/components/card/index.js
+++ packages/react-ui/src/components/card/index.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Element } from '../../primitives'
 import { styles } from './card.styles'
 import { merge } from '../../utils'
 
-const Card = React.forwardRef(function Card({ css, ...props }, ref) {
+const Card = React.forwardRef(function Card({ fullWidth, css, ...props }, ref) {
+  let width = fullWidth ? '100%' : 'auto'
+
   return (
     <Element
       ref={ref}
       as="div"
       component="Card"
-      css={merge(styles.Card, css)}
+      css={merge(styles.Card, { width }, css)}
       {...props}
     />
   )
 })
+
+Card.propTypes = {
+  /** Description of an card prop */
+  fullWidth: PropTypes.bool
+}
 
 export { Card }
