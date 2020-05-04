5d746d923e7d097e79cb6de590ba0b2ca6bbaf19
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-05-04 21:34:44 +0200

    add a changelog

--- packages/docs/src/pages/changelog/index.js
+++ packages/docs/src/pages/changelog/index.js
@@ -1,14 +1,113 @@
 import React from 'react'
 import { Text, Link, Avatar, Stack } from 'react-ui'
+import VisuallyHidden from '@reach/visually-hidden'
 import { Page, Section, List } from '../../components'
 
 const Changelog = () => {
   return (
     <Page title="Changelog">
       <Section>
-        <Link variant="body" href="#1.0.0-beta.24" marginBottom={4}>
-          1.0.0-beta.24
-        </Link>
+        <Stack align="center" justify="space-between" marginBottom={4}>
+          <Link variant="body" href="#1.0.0-beta.25">
+            1.0.0-beta.25{' '}
+          </Link>
+          <Contributors
+            authors={[
+              'tarang9211',
+              'rubenmoya',
+              'ankibalyan',
+              'sumdook',
+              'neves',
+              'SaraVieira',
+              'siddharthkp'
+            ]}
+          />
+        </Stack>
+        <Stack as={List} direction="vertical" gap={1} marginBottom={4}>
+          <Text size={3} color="text.subtle">
+            Additions:
+          </Text>
+          <Change
+            pr="46"
+            description="Checkbox - first draft"
+            author="tarang9211"
+          />
+        </Stack>
+        <Stack as={List} direction="vertical" gap={1} marginBottom={4}>
+          <Text size={3} color="text.subtle">
+            Improvements:
+          </Text>
+          <Change
+            pr="33"
+            description="Add forwardRef to all components"
+            author="rubenmoya"
+          />
+          <Change
+            pr="36"
+            description="Added fullwidth to the button prop"
+            author="ankibalyan"
+          />
+          <Change
+            pr="52"
+            description="Use bundlesize2 preview"
+            author="siddharthkp"
+          />
+          <Change
+            pr="53"
+            description="Docs: Add size docs for Text #53"
+            author="siddharthkp"
+          />
+        </Stack>
+        <Stack as={List} direction="vertical" gap={1} marginBottom={4}>
+          <Text size={3} color="text.subtle">
+            Fixes:
+          </Text>
+          <Change
+            pr="43"
+            description="Docs: Fixed links to theme files"
+            author="sumdook"
+          />
+          <Change
+            pr="56"
+            description="Respect required of an Input inside Form.Field"
+            author="siddharthkp"
+          />
+        </Stack>
+
+        <Stack as="span" align="center" gap={1} marginBottom={4}>
+          <LinkedAvatar author="neves" />
+          <LinkedAvatar author="SaraVieira" />
+          <Text size={3} color="text.subtle">
+            Thanks to{' '}
+            <Link
+              href={`https://github.com/neves`}
+              target="_blank"
+              variant="subtle"
+            >
+              neves
+            </Link>{' '}
+            and{' '}
+            <Link
+              href={`https://github.com/SaraVieira`}
+              target="_blank"
+              variant="subtle"
+            >
+              SaraVieira
+            </Link>{' '}
+            for their feedback.
+          </Text>
+        </Stack>
+      </Section>
+
+      <Section>
+        <Stack align="center" justify="space-between" marginBottom={4}>
+          <Link variant="body" href="#1.0.0-beta.24">
+            1.0.0-beta.24
+          </Link>
+          <Contributors
+            authors={['rubenmoya', 'Andarist', 'abhinav-anshul', 'siddharthkp']}
+          />
+        </Stack>
         <Stack as={List} direction="vertical" gap={1} marginBottom={4}>
           <Text size={3} color="text.subtle">
             Additions:
@@ -48,9 +147,20 @@ const Changelog = () => {
       </Section>
 
       <Section>
-        <Link variant="body" href="#1.0.0-beta.24" marginBottom={4}>
-          Earlier versions
-        </Link>
+        <Stack justify="space-between" align="center" marginBottom={4}>
+          <Link variant="body" href="#earlier-versions">
+            Earlier versions
+          </Link>
+          <Contributors
+            authors={[
+              'arpitbatra123',
+              'ggoodman',
+              'tatchi',
+              'mbj36',
+              'rudeayelo'
+            ]}
+          />
+        </Stack>
         <Stack as={List} direction="vertical" gap={1} marginBottom={4}>
           <Text size={3} color="text.subtle">
             Fixes:
@@ -104,15 +214,36 @@ const Change = ({ pr, description, author }) => (
       </Link>{' '}
       {description}
     </Text>
-    <Link
-      href={`https://github.com/${author}`}
-      target="_blank"
-      variant="subtle"
-    >
-      <Stack align="center" gap={1}>
-        <Avatar size="small" src={`https://github.com/${author}.png`} />
-        <Text size={3}>{author}</Text>
-      </Stack>
-    </Link>
+    <LinkedAvatar author={author} showName />
   </Stack>
+)
+
+const Contributors = ({ authors }) => (
+  <Stack gap={2} align="center">
+    <Text size={3} color="text.subtle">
+      {authors.length} contributors
+    </Text>
+    <Stack gap={1}>
+      {authors.map(author => (
+        <LinkedAvatar key={author} author={author} />
+      ))}
+    </Stack>
+  </Stack>
+)
+
+const LinkedAvatar = ({ author, showName }) => (
+  <Link href={`https://github.com/${author}`} target="_blank" variant="subtle">
+    <Stack as="span" align="center" gap={showName ? 1 : 0}>
+      <Avatar
+        size="small"
+        title={author}
+        src={`https://github.com/${author}.png`}
+      />
+      {showName ? (
+        <Text size={3}>{author}</Text>
+      ) : (
+        <VisuallyHidden>{author}</VisuallyHidden>
+      )}
+    </Stack>
+  </Link>
 )
