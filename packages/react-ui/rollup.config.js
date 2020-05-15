a13e438e027b4e969abe9f0b5575774e03293e66
Ankit Kumar <ankit.kr.balyan@gmail.com>
2020-05-15 19:51:37 +0530

    Added prod & dev build scripts to reduces bundle size in production

--- packages/react-ui/rollup.config.js
+++ packages/react-ui/rollup.config.js
@@ -3,42 +3,43 @@ import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import json from 'rollup-plugin-json'
+import { terser } from 'rollup-plugin-terser'
+import reactRemovePropType from 'babel-plugin-transform-react-remove-prop-types'
 
 import pkg from './package.json'
 
-const plugins = [
-  external({
-    includeDependencies: true
-  }),
-  resolve(),
-  babel({
-    presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
-    exclude: 'node_modules/**'
-  }),
-  commonjs(),
-  json()
-]
+const getPlugins = (isProd) => {
+  return [
+    external({ includeDependencies: true }),
+    resolve(),
+    babel({
+      presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+      plugins: [isProd && [reactRemovePropType, { mode: 'wrap' }]].filter(Boolean), // using wrap to include propTypes only when process.env.NODE_ENV !== "production" 
+      exclude: 'node_modules/**'
+    }),
+    commonjs(),
+    json(),
+    isProd && terser()
+  ]
+}
+
+const cjs = [{
+  input: pkg.source,
+  output: { file: `dist/${pkg.name}.js`, format: 'cjs', sourcemap: true, esModule: false },
+  plugins: getPlugins(false) // dev env plugins
+}, {
+  input: pkg.source,
+  output: { file: `dist/${pkg.name}.min.js`, format: 'cjs', sourcemap: true },
+  plugins: getPlugins(true) // prod env plugins
+}];
 
 export default [
-  {
-    input: pkg.source,
-    output: [{ file: pkg.main, format: 'cjs' }],
-    plugins
-  },
-  {
+  ...cjs, {
     input: ['src/themes/base.js', 'src/themes/light.js', 'src/themes/dark.js'],
     output: [
-      {
-        dir: 'dist/themes/',
-        format: 'cjs',
-        exports: 'named'
-      },
-      {
-        dir: 'themes',
-        format: 'cjs',
-        exports: 'named'
-      }
+      { dir: 'dist/themes/', format: 'cjs', exports: 'named' },
+      { dir: 'themes', format: 'cjs', exports: 'named' }
     ],
-    plugins
+    plugins: getPlugins(true)
   }
 ]
